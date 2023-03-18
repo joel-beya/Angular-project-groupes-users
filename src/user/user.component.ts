c67a6c7d38c7f13d034577f571f397b5f5882143
Joël <joelbeya418@gmail.com>
2023-03-18 14:14:49 +0100

    MAJ

--- src/user/user.component.ts
+++ src/user/user.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Groupe } from 'src/groupe';
 
 @Component({
   selector: 'app-user',
@@ -9,6 +10,7 @@ export class UserComponent implements OnInit {
 
   id_user : string="";
   mdp : string="";
+  @Input() groupes: Groupe[] = [];
 
   constructor() { }
 
@@ -17,4 +19,22 @@ export class UserComponent implements OnInit {
   getId_user(){
     return this.id_user;
   }
+
+  rejoindreGroupe(idg : number){
+
+      this.groupes.forEach(groupe => {
+        if(groupe.id_groupe === idg ){
+          if(groupe.liste_user.length-1 <= groupe.nbr_user){
+            groupe.liste_user.push(this.id_user);
+          }
+          else{
+            console.log("Le groupe est complet");
+          }
+
+        }
+
+
+      })
+  }
+
 }
